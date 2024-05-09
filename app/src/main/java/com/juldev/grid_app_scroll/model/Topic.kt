0037f558b73372ed1752d71f17a8b304c6b96fda
M-Julius <mjulius.dev@gmail.com>
2024-05-09 17:52:47 +0700

    first commit

---
+++ app/src/main/java/com/juldev/grid_app_scroll/model/Topic.kt
@@ -0,0 +1,11 @@
+package com.juldev.grid_app_scroll.model
+
+import android.media.Image
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
